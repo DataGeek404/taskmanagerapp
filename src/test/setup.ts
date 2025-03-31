7c05d004926bd0d524dcf3c57bd0e6a961679920
DataGeek404 <techjayprivate@gmail.com>
2025-03-31 11:11:26 +0300

    tests setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -7,7 +7,7 @@ import matchers from '@testing-library/jest-dom/matchers';
 // Extend Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
-// Run cleanup after each test case (e.g. clearing jsdom)
+// Run cleanup after each test case (e.g. clearing jsdom e.t.c)
 afterEach(() => {
   cleanup();
 });
